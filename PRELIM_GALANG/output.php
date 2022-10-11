f7ca0cfdba82b4ec1212acaac0be47e68a06cb0f
jstnrn <115512420+jstnrn@users.noreply.github.com>
2022-10-11 10:30:35 +0800

    Add files via upload

---
+++ PRELIM_GALANG/output.php
@@ -0,0 +1,176 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class="supernova"><head><script>console.warn("Server Side Rendering => render-from: frontend");</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="alternate" type="application/json+oembed" href="https://www.jotform.com/oembed/?format=json&amp;url=https%3A%2F%2Fform.jotform.com%2F222827554799473" title="oEmbed Form">
+<link rel="alternate" type="text/xml+oembed" href="https://www.jotform.com/oembed/?format=xml&amp;url=https%3A%2F%2Fform.jotform.com%2F222827554799473" title="oEmbed Form">
+<meta property="og:title" content="New Customer Registration Form" >
+<meta property="og:url" content="https://form.jotform.com/222827554799473" >
+<meta property="og:description" content="Please click the link to complete this form." >
+<meta name="slack-app-id" content="AHNMASS8M">
+<link rel="shortcut icon" href="https://cdn.jotfor.ms/assets/img/favicons/favicon-2021.svg">
+<meta property="og:image" content="https://cdn.jotfor.ms/assets/img/favicons/favicon-2021.svg" />
+<link rel="canonical" href="https://form.jotform.com/222827554799473" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=1" />
+<meta name="HandheldFriendly" content="true" />
+<title>New Customer Registration Form</title>
+<style type="text/css">@media print{.form-section{display:inline!important}.form-pagebreak{display:none!important}.form-section-closed{height:auto!important}.page-section{position:initial!important}}</style>
+<link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?themeRevisionID=5eb3b4ae85bd2e1e2966db96"/>
+<link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/css/styles/payment/payment_styles.css?3.3.36340" />
+<link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/css/styles/payment/payment_feature.css?3.3.36340" />
+<script src="https://cdn01.jotfor.ms/static/prototype.forms.js?3.3.36340" type="text/javascript"></script>
+<script src="https://cdn02.jotfor.ms/static/jotform.forms.js?3.3.36340" type="text/javascript"></script>
+<script src="https://cdn03.jotfor.ms/js/vendor/jquery-1.8.0.min.js?v=3.3.36340" type="text/javascript"></script>
+<script defer src="https://cdn01.jotfor.ms/js/vendor/maskedinput.min.js?v=3.3.36340" type="text/javascript"></script>
+<script defer src="https://cdn02.jotfor.ms/js/vendor/jquery.maskedinput.min.js?v=3.3.36340" type="text/javascript"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/punycode/1.4.1/punycode.js"></script>
+<script type="text/javascript">	JotForm.newDefaultTheme = true;
+	JotForm.extendsNewTheme = false;
+	JotForm.singleProduct = false;
+	JotForm.newPaymentUIForNewCreatedForms = false;
+	JotForm.newPaymentUI = true;
+
+   JotForm.setConditions([{"action":[{"field":"9","visibility":"Show","id":"action_0_1579704830003"}],"id":"1579704830003","index":"0","link":"Any","priority":"0","terms":[{"field":"8","operator":"equals","value":"Other (Please specify...)"}],"type":"field"}]);
+	JotForm.init(function(){
+	/*INIT-START*/
+      JotForm.setPhoneMaskingValidator( 'input_5_full', '(###) ###-####' );
+      setTimeout(function() {
+          $('input_6').hint('ex: email@yahoo.com');
+       }, 20);
+if (window.JotForm && JotForm.accessible) $('input_9').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_11').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_12').setAttribute('tabindex',0);
+	/*INIT-END*/
+	});
+
+   JotForm.prepareCalculationsOnTheFly([null,{"name":"clickTo","qid":"1","text":"Customer Details:","type":"control_head"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"fullName3","qid":"3","text":"Full Name","type":"control_fullname"},{"name":"address4","qid":"4","text":"Address","type":"control_address"},{"name":"phoneNumber5","qid":"5","text":"Phone Number","type":"control_phone"},{"name":"email6","qid":"6","subLabel":"example@example.com","text":"E-mail","type":"control_email"},{"name":"clickTo7","qid":"7","text":"","type":"control_text"},{"name":"howDid8","qid":"8","text":"How did you hear about us?","type":"control_dropdown"},{"name":"other","qid":"9","text":"Other","type":"control_textbox"},null,{"name":"feedbackAbout11","qid":"11","text":"Feedback about us:","type":"control_textarea"},{"name":"suggestionsIf","qid":"12","text":"Suggestions if any for further improvement:","type":"control_textarea"},{"name":"willYou","qid":"13","text":"Will you be willing to recommend us?","type":"control_checkbox"},{"name":"pleaseGive14","qid":"14","text":"Please give reference of any two people whom you feel:","type":"control_matrix"}]);
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"clickTo","qid":"1","text":"Customer Details:","type":"control_head"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"fullName3","qid":"3","text":"Full Name","type":"control_fullname"},{"name":"address4","qid":"4","text":"Address","type":"control_address"},{"name":"phoneNumber5","qid":"5","text":"Phone Number","type":"control_phone"},{"name":"email6","qid":"6","subLabel":"example@example.com","text":"E-mail","type":"control_email"},{"name":"clickTo7","qid":"7","text":"","type":"control_text"},{"name":"howDid8","qid":"8","text":"How did you hear about us?","type":"control_dropdown"},{"name":"other","qid":"9","text":"Other","type":"control_textbox"},null,{"name":"feedbackAbout11","qid":"11","text":"Feedback about us:","type":"control_textarea"},{"name":"suggestionsIf","qid":"12","text":"Suggestions if any for further improvement:","type":"control_textarea"},{"name":"willYou","qid":"13","text":"Will you be willing to recommend us?","type":"control_checkbox"},{"name":"pleaseGive14","qid":"14","text":"Please give reference of any two people whom you feel:","type":"control_matrix"}]);}, 20); 
+</script>
+</head>
+<body>
+<form class="jotform-form" action="output.php" method="post" name="form_222827554799473" id="222827554799473" accept-charset="utf-8" autocomplete="on">
+  <input type="hidden" name="formID" value="222827554799473" />
+  <input type="hidden" id="JWTContainer" value="" />
+  <input type="hidden" id="cardinalOrderNumber" value="" />
+  <div role="main" class="form-all">
+    <style>
+      .form-all:before { background: none;}
+    </style>
+    <ul class="form-section page-section">
+      <li id="cid_1" class="form-input-wide" data-type="control_head">
+        <div class="form-header-group  header-large">
+          <div class="header-text httal htvam">
+            <center>
+            <h1 style="color: #FF7A59" text-align: center; id="header_1" class="form-header" data-component="header">
+              CUSTOMER DETAILS
+             
+            </h1>
+            </center>
+            <br>
+            <br>
+            <?php
+          
+if (!empty("submit")){
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$addline1 =$_POST['add1'];
+$addline2 =$_POST['add2'];
+$city =$_POST['q4_address42'];
+$scity =$_POST['q4_address43'];
+$pcity =$_POST['q4_address44'];
+$pnum =$_POST['q5_phoneNumber5'];
+$email =$_POST['q6_email6'];
+$hear =$_POST['q8_howDid8'];
+$other =$_POST['q9_other'];
+$fback =$_POST['q11_feedbackAbout11'];
+$suggest =$_POST['q12_suggestionsIf'];
+$willing =$_POST['q13_willYou'];
+$pgive1 =$_POST['p1'];
+$pgive11 =$_POST['p11'];
+$pgive12 =$_POST['p12'];
+$pgive21 =$_POST['p2'];
+$pgive22 =$_POST['p22'];
+$pgive23 =$_POST['p23'];
+
+
+
+echo '<span style="color: black; font-size: 20px; text-align: left;"> ' ." <b> First Name </b>: $fname <br/>  ";
+echo " <b>Last Name </b> : $lname <br/> ";
+echo "<b>Street Address </b> : $addline1 <br/> ";
+echo " <b>Street Address 2 </b> : $addline2 <br/> ";
+echo " <b>City</b> : $city <br/> ";
+echo " <b>State/Province</b> : $scity <br/> ";
+echo " <b>Postal / Zipcode</b> : $pcity <br/> ";
+echo " <b>Phone Number</b> : $pnum <br/> ";
+echo " <b>Email</b> : $email <br/> ";
+echo " <b></b> <br/> ";
+echo " <b></b> <br/> ";
+echo " <b>How did you hear about us?</b> : $hear <br/> ";
+echo " <b>Feedback about us: </b> : $fback <br/> ";
+echo " <b>Suggestions if any for further improvement: </b> : $suggest <br/> ";
+echo " <b>Will you be willing to recommend us?</b> : $willing <br/> ";
+echo " <b></b> <br/> ";
+echo " <b>Please give reference of any two people whom you feel</b> : <br/> ";
+echo " <b>_________________________________________________________________ </b>  <br/> ";
+echo " <b>Person 1 </b> : <br/> ";
+echo " <b>_________________________________________________________________ </b>  <br/> ";
+echo " <b>Fullname </b> : $pgive1 <br/> ";
+echo " <b>Address </b> : $pgive11 <br/> ";
+echo " <b>Contact Number </b> : $pgive12 <br/> ";
+echo " <b>_________________________________________________________________ </b>  <br/> ";
+echo " <b>Person 2 </b> : <br/> ";
+echo " <b>_________________________________________________________________ </b>  <br/> ";
+echo " <b>Fullname </b> : $pgive21  <br/> ";
+echo " <b>Address </b> :  $pgive22 <br/> ";
+echo " <b>Contact Number </b> : $pgive23  <br/> ";
+
+
+
+
+
+
+}
+
+
+?>
+          </div>
+        </div>
+      </li>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <script>
+  JotForm.showJotFormPowered = "new_footer";
+  </script>
+  <script>
+  JotForm.poweredByText = "Powered by Jotform";
+  </script>
+  <input type="hidden" class="simple_spc" id="simple_spc" name="simple_spc" value="222827554799473" />
+  <script type="text/javascript">
+  var all_spc = document.querySelectorAll("form[id='222827554799473'] .si" + "mple" + "_spc");
+for (var i = 0; i < all_spc.length; i++)
+{
+  all_spc[i].value = "222827554799473-222827554799473";
+}
+  </script>
+  <div class="formFooter-heightMask">
+  </div>
+  <div class="formFooter f6 branding21">
+    <div class="formFooter-wrapper formFooter-leftSide">
+      <a href="https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=222827554799473&utm_content=jotform_logo&utm_campaign=powered_by_jotform_le" target="_blank" class="formFooter-logoLink"><img class="formFooter-logo" src="https://cdn.jotfor.ms/assets/img/logo2021/jotform-logo-white.svg" alt="Jotform Logo" style="height: 44px;"></a>
+    </div>
+    <div class="formFooter-wrapper formFooter-rightSide">
+      <span class="formFooter-text">
+        Now create your own Jotform - It's free!
+      </span>
+      <a class="formFooter-button" href="https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=222827554799473&utm_content=jotform_button&utm_campaign=powered_by_jotform_le" target="_blank">Create your own Jotform</a>
+    </div>
+  </div>
+</form></body>
+</html>
+<script type="text/javascript">JotForm.ownerView=true;</script><script src="https://cdn.jotfor.ms//js/vendor/smoothscroll.min.js?v=3.3.36340"></script>
+<script src="https://cdn.jotfor.ms//js/errorNavigation.js?v=3.3.36340"></script>
